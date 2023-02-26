2970b25440572e73641c631b1e6ead6594d7a860
Entity233 <2966388213@qq.com>
2023-02-27 00:20:11 +0800

    2.27

--- docs/.vuepress/Config/sidebar.ts
+++ docs/.vuepress/Config/sidebar.ts
@@ -14,6 +14,7 @@ const sidebar:SidebarConfig =  {
                 '/js/CircleCollision.md',
                 '/js/canvasClock.md',
                 '/js/canvas.md',
+                'canvasPieChart.md',
                 'dom.md',
                 'IntersectionObserver.md',
                 'event.md'
