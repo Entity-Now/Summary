22f274b5000097cce1410dbe332b6c227ac5fa61
Entity-Now <51899658+Entity-Now@users.noreply.github.com>
2023-08-25 07:02:16 +0000

    Deploying to gh-pages from @ Entity-Now/Summary@d98792452518d20330b12e361a55012e30d7b4fd 🚀

---
+++ assets/crossType.html-6bd35d08.js
@@ -0,0 +1,19 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as a,f as e,a as t}from"./app-d69bb520.js";const p={},o=t(`<h1 id="交叉类型" tabindex="-1"><a class="header-anchor" href="#交叉类型" aria-hidden="true">#</a> 交叉类型</h1><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token comment">// 交叉类型</span>
+
+<span class="token keyword">interface</span> <span class="token class-name">bird</span><span class="token punctuation">{</span>
+    <span class="token literal-property property">fly</span><span class="token operator">:</span>string<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token keyword">interface</span> <span class="token class-name">fish</span><span class="token punctuation">{</span>
+    <span class="token literal-property property">swim</span><span class="token operator">:</span>string<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token comment">// 交叉类型，</span>
+type cross <span class="token operator">=</span> bird <span class="token operator">&amp;</span> fish<span class="token punctuation">;</span>
+<span class="token keyword">let</span> <span class="token literal-property property">test</span><span class="token operator">:</span>cross <span class="token operator">=</span> <span class="token punctuation">{</span>
+    <span class="token literal-property property">fly</span><span class="token operator">:</span><span class="token string">&quot;bird fly&quot;</span><span class="token punctuation">,</span>
+    <span class="token literal-property property">swim</span><span class="token operator">:</span><span class="token string">&quot;fish swim&quot;</span>
+<span class="token punctuation">}</span>
+console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>test<span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function c(i,l){return n(),a("div",null,[e(`
+ * @作者: 14770137
+ * @Date: 2022-10-13 14:34:23
+`),o])}const d=s(p,[["render",c],["__file","crossType.html.vue"]]);export{d as default};
