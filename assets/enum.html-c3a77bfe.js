3d49e01c6866667dfc0f4721e6add69cb5910cf6
Entity-Now <51899658+Entity-Now@users.noreply.github.com>
2023-11-03 05:53:11 +0000

    Deploying to gh-pages from @ Entity-Now/Summary@03b5e93a062dbc7a4e0813d7df71581b61149f92 🚀

---
+++ assets/enum.html-c3a77bfe.js
@@ -0,0 +1,22 @@
+import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{o as s,c as a,a as e}from"./app-93d39420.js";const t={},c=e(`<h1 id="枚举-enum" tabindex="-1"><a class="header-anchor" href="#枚举-enum" aria-hidden="true">#</a> 枚举 enum</h1><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">enum</span> Direction<span class="token punctuation">{</span>
+    up <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">,</span>
+    Down<span class="token punctuation">,</span>
+    left<span class="token punctuation">,</span>
+    right
+<span class="token punctuation">}</span>
+<span class="token keyword">enum</span> FileAccess <span class="token punctuation">{</span>
+    <span class="token comment">// constant members</span>
+    None<span class="token punctuation">,</span>
+    Read    <span class="token operator">=</span> <span class="token number">1</span> <span class="token operator">&lt;&lt;</span> <span class="token number">1</span><span class="token punctuation">,</span>
+    Write   <span class="token operator">=</span> <span class="token number">1</span> <span class="token operator">&lt;&lt;</span> <span class="token number">2</span><span class="token punctuation">,</span>
+    ReadWrite  <span class="token operator">=</span> Read <span class="token operator">|</span> Write<span class="token punctuation">,</span>
+    <span class="token comment">// computed member</span>
+    <span class="token constant">G</span> <span class="token operator">=</span> <span class="token string">&quot;123&quot;</span><span class="token punctuation">.</span>length
+<span class="token punctuation">}</span>
+console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>FileAccess<span class="token punctuation">.</span>ReadWrite<span class="token punctuation">)</span><span class="token punctuation">;</span> <span class="token comment">// output : 6</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="外部枚举-declare-enum" tabindex="-1"><a class="header-anchor" href="#外部枚举-declare-enum" aria-hidden="true">#</a> 外部枚举 declare enum</h2><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token comment">// 外部枚举，用来描述已经存在的枚举类型的形状</span>
+declare <span class="token keyword">enum</span> Direction<span class="token punctuation">{</span>
+    <span class="token constant">C</span> <span class="token operator">=</span> <span class="token number">2</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),p=[c];function i(o,l){return s(),a("div",null,p)}const d=n(t,[["render",i],["__file","enum.html.vue"]]);export{d as default};
---
+++ assets/vuepress.html-3d592c5e.js
@@ -0,0 +1,20 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,f as r,a as s}from"./app-93d39420.js";const a={},d=s(`<h1 id="vuepress-v2" tabindex="-1"><a class="header-anchor" href="#vuepress-v2" aria-hidden="true">#</a> vuepress v2</h1><h2 id="使用frontmatter" tabindex="-1"><a class="header-anchor" href="#使用frontmatter" aria-hidden="true">#</a> 使用frontmatter</h2><div class="language-line-number line-numbers-mode" data-ext="line-number"><pre class="language-line-number"><code>---
+title:  主页
+date:   2022-10-24
+description: Entity233的个人博客，在这里记录着众多编程语言的开发经验。
+lastUpdated:    true
+# - 一维数组
+# -- 二维数组
+# 二维数据
+# - [&#39;array&#39;,{object:ok}] 
+sidebar:
+    - text: JavaScript
+      link: /js
+    - text: git
+      link: /git
+---
+
+</code></pre><div class="highlight-lines"><br><br><br><br><br><br><br><br><div class="highlight-line"> </div><br><br><br><br><br><br><br></div><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3);function l(t,v){return i(),n("div",null,[r(`
+ * @作者: 14770137
+ * @Date: 2022-10-25 03:51:45
+`),d])}const u=e(a,[["render",l],["__file","vuepress.html.vue"]]);export{u as default};
